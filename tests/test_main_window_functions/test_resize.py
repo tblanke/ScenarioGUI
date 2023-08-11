ce6a39e463df8d60446610c83d25a5553921bf30
tblanke <86232208+tblanke@users.noreply.github.com>
2023-08-11 09:14:19 +0200

    Update test_resize.py

--- tests/test_main_window_functions/test_resize.py
+++ tests/test_main_window_functions/test_resize.py
@@ -1,4 +1,5 @@
 import PySide6.QtCore as QtC
+import PySide6.QtGui as QtG
 
 from ..starting_closing_tests import close_tests, start_tests
 
@@ -15,12 +16,10 @@ def test_resize_event_button_sizes(qtbot):
     # init gui window
     main_window = start_tests(qtbot)
     main_window.dia.setFixedSize(QtC.QSize(150, 150))
-    main_window.resizeEvent(None)
-    assert main_window.size_push_s.height() < 75
-    assert main_window.size_push_b.height() < 75
+    main_window.resizeEvent(QtG.QResizeEvent(QtC.QSize(150, 150), QtC.QSize(1500, 1500)))
     assert main_window.size_b.height() < 48
     main_window.dia.setFixedSize(QtC.QSize(1500, 1500))
-    main_window.resizeEvent(None)
+    main_window.resizeEvent(QtG.QResizeEvent(QtC.QSize(1500, 1500), QtC.QSize(150, 150)))
     assert main_window.size_push_s.height() == 75
     assert main_window.size_push_b.height() == 75
     assert main_window.size_b.height() == 48
